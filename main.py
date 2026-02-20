92af69cf827f785021e73dfb0a81ea820b3bc7a8
Shivani Kamble <kambleshivani78@gmail.com>
2026-02-20 10:39:31 +0530

    Fixed merge conflict

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from fastapi import FastAPI
 from database import engine, Base
 from routers.shortener import router as shortener_router
@@ -8,7 +8,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="URL Shortener")
 app.include_router(shortener_router)
 
-=======
+
 from fastapi import FastAPI
 from database import engine, Base
 from routers.shortener import router as shortener_router
@@ -17,5 +17,3 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="URL Shortener")
 app.include_router(shortener_router)
-
->>>>>>> 5fa64b90cb376dbbbbf9b7d139e565d2b14f5722
